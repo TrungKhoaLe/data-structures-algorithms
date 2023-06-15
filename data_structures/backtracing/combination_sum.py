023d4fb1107958e3fe8a26380e65aabfd820df57
TrungKhoaLe <khoaletrung@gmail.com>
2023-06-16 01:44:05 +0700

    [Backtracking] combination sum

---
+++ data_structures/backtracing/combination_sum.py
@@ -0,0 +1,27 @@
+from typing import List
+
+
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        self.result = []
+        self._backtrack(0, candidates, [], 0, target)
+        return self.result
+
+    def _backtrack(self, index_position: int, candidates: List[int], subset: List[int], total: int, target: int):
+        if total == target:
+            self.result.append(subset.copy())
+            return
+        if index_position >= len(candidates) or total > target:
+            return
+
+        subset.append(candidates[index_position])
+        self._backtrack(index_position, candidates, subset, total + candidates[index_position], target)
+        subset.pop()
+        self._backtrack(index_position + 1, candidates, subset, total, target)
+
+
+if __name__ == '__main__':
+    example_1 = [2, 3, 6, 7]
+    target = 7
+    s = Solution()
+    print(s.combinationSum(example_1, target))
